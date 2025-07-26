5d73378a556bcbf9811414bf4aeb18877ba2a1a7
delowarshizar <husain22205101453@diu.edu.bd>
2025-07-26 21:14:45 +0600

    variable more

--- variable.js
+++ variable.js
@@ -2,3 +2,14 @@ var age = 15;
 var height = 6;
  
 console.log(age,height)
+
+var name = "delwoar Husain";
+var country = 'bangladesh'
+console.log(name,country)
+
+var istrue = true;
+var isfalse  = false;
+console.log(isfalse)
+
+console.log(typeof country)
+
