a6d4dc9feebc9d6107a27efd93fecced9a5cb8f9
delowarshizar <husain22205101453@diu.edu.bd>
2025-07-26 21:52:14 +0600

    parse and tofixed

---
+++ typeconv.js
@@ -0,0 +1,19 @@
+var num1 = 1;
+var num2 = '2.4';
+var num3 = '30';
+
+
+var num4 = parseInt(num3)
+var num5 = parseFloat(num2)
+
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+
+var a=0.1;
+var b=0.2;
+var total=a+b;
+console.log(total.toFixed(5))
