a01107035a319eb64a08478764866c73bfaa315b
delowarshizar <husain22205101453@diu.edu.bd>
2025-08-01 02:32:51 +0600

    Object basic

---
+++ object.js
@@ -0,0 +1,9 @@
+const university = {
+    name: "Daffodil International university",
+    date:1905,
+    department:{
+        name:['cse','eee','bba'],
+        fee:30334,
+    }
+}
+console.log( typeof university)
