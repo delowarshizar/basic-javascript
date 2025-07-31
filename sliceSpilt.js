e8b817e6a7895444be4065119f7a089fac398148
delowarshizar <husain22205101453@diu.edu.bd>
2025-08-01 01:51:55 +0600

    string more topic

---
+++ sliceSpilt.js
@@ -0,0 +1,10 @@
+const song = "amar sonar bangla ami tomay valobasi";
+console.log(song.slice(2,6));
+console.log(song.split(' '));
+
+const song2 = [ 'amar', 'sonar', 'bangla', 'ami', 'tomay', 'valobasi' ];
+console.log(song2.join(','));
+const first = "Delowar";
+const last = "Caesar";
+
+console.log(first.concat(' ').concat(last))
