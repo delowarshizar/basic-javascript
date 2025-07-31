b7dc91fe94b6d6f268fb90455d21890003824d3f
delowarshizar <husain22205101453@diu.edu.bd>
2025-08-01 01:37:01 +0600

    string practice

---
+++ string.js
@@ -0,0 +1,12 @@
+const name1 ="delowar";
+const name2 = "  delowar";
+
+// if (name1.toLowerCase() === name2.toLowerCase())
+if (name1.trim() === name2.trim())
+{
+console.log("same name");
+}
+
+else{
+    console.log("not same name")
+}
