fcd6187afcf1edfce6acab8c5c93bebe3a14611e
delowarshizar <husain22205101453@diu.edu.bd>
2025-08-02 22:52:40 +0600

    function

---
+++ function.js
@@ -0,0 +1,28 @@
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+function sub(num1, num2) {
+    const result = num1 - num2;
+    return result;
+}
+function mul(num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
+function div(num1, num2) {
+    const result = num1 / num2;
+    return result;
+}
+
+const addR = add(3,4);
+const subR = sub(3,4);
+const mulR = mul(3,4);
+const divR = div(3,4);
+
+
+console.log(addR);
+console.log(subR);
+console.log(mulR);
+console.log(divR);
