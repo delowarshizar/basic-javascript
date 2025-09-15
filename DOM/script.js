c7ee9b18e202691ff460924b955b805b1fa2aa17
delowarshizar <husain22205101453@diu.edu.bd>
2025-09-15 20:10:06 +0600

    lecture 2

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,12 @@
 console.log("hello from outside")
 console.log(document)
+const licomp = document.getElementsByTagName('li')
+console.log(licomp)
+for(const li of licomp){
+    console.log(li.innerText)
+}
+
+const heading1 = document.getElementsByTagName('h1')
+for(const h1 of heading1){
+    console.log(h1.innerText)
+}
