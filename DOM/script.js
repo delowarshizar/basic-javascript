cd8177ec9dec223f46896e105a80eb88daf02eb2
delowarshizar <husain22205101453@diu.edu.bd>
2025-09-15 21:48:36 +0600

    id and class dom

--- DOM/script.js
+++ DOM/script.js
@@ -10,3 +10,11 @@ const heading1 = document.getElementsByTagName('h1')
 for(const h1 of heading1){
     console.log(h1.innerText)
 }
+const foodlove = document.getElementById('food')
+console.log(foodlove.innerText)
+const itemslist = document.getElementsByClassName('items')
+ for( const li of itemslist)
+ {
+    console.log(li.innerText)
+ }
+
