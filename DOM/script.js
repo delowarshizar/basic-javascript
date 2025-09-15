641c0fbbf83070d1ccadadaf4b81f8935cfd2756
delowarshizar <husain22205101453@diu.edu.bd>
2025-09-15 23:55:19 +0600

    create and adding element of html using js

--- DOM/script.js
+++ DOM/script.js
@@ -19,26 +19,33 @@ for (const li of itemslist) {
   console.log(li.innerText);
 }
 
-const fevitems = document.querySelectorAll('#food .items')
-console.log(fevitems)
+const fevitems = document.querySelectorAll("#food .items");
+console.log(fevitems);
 
-for(const item of fevitems)
-{
-    console.log(item.innerText)
+for (const item of fevitems) {
+  console.log(item.innerText);
 }
 
-const sectionlist = document.querySelectorAll('section')
-console.log(sectionlist)
-// for(const section of sectionlist)
-// {
-//     console.log(section)
-//     section.style.backgroundColor='lightblue'
-//     section.style.padding = '5px'
-//     section.style.borderRadius = '10px'
-//     section.style.border = '2px solid green'
-//     section.style.margin = '5px'
-// }
+const sectionlist = document.querySelectorAll("section");
+console.log(sectionlist);
 for(const section of sectionlist)
 {
-    section.classList.add('section-card')
+    console.log(section)
+    section.style.backgroundColor='lightblue'
+    section.style.padding = '5px'
+    section.style.borderRadius = '10px'
+    section.style.border = '2px solid green'
+    section.style.margin = '5px'
 }
+// for (const section of sectionlist) {
+//   section.classList.add("section-card");
+// }
+// adding element using js
+const newchild = document.createElement("li");
+newchild.innerText = "Delowar Husain";
+console.log(newchild.innerText);
+
+const footballer = document.getElementById("Player");
+console.log(footballer);
+
+footballer.appendChild(newchild);
