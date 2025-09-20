a70ee9e94d27144000b9eaee89dd9494625b0760
delowarshizar <husain22205101453@diu.edu.bd>
2025-09-20 12:59:32 +0600

    identation

--- DOM Events/script.js
+++ DOM Events/script.js
@@ -1,26 +1,24 @@
 console.log("file connected successfully");
 
-function makePink()
-{
-    document.body.style.backgroundColor = 'pink';
+function makePink() {
+  document.body.style.backgroundColor = "pink";
 }
 
-function makeGreen ()
-{
-    document.body.style.backgroundColor = 'Green';
+function makeGreen() {
+  document.body.style.backgroundColor = "Green";
 }
 
 // option 3
 
-const makePurple = document.getElementById('btn-make-purple');
+const makePurple = document.getElementById("btn-make-purple");
 // console.log(makePurple);
 makePurple.onclick = makeBgPurple;
-function makeBgPurple()
-{
-    document.body.style.backgroundColor = 'purple'
+function makeBgPurple() {
+  document.body.style.backgroundColor = "purple";
 }
 
-document.getElementById('btn-make-yellow')
-    .addEventListener('click',function(){
-        document.body.style.backgroundColor = 'yellow'
-    })
+document
+  .getElementById("btn-make-yellow")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "yellow";
+  });
--- arrayModification.js
+++ arrayModification.js
@@ -1,11 +1,11 @@
-const items = ['apple','banana', 'Orange','jackfruit',35,45.4];
+const items = ["apple", "banana", "Orange", "jackfruit", 35, 45.4];
 const delowar = 225;
 
-console.log(items.includes('apple'));
-console.log(items.includes('juice'));
+console.log(items.includes("apple"));
+console.log(items.includes("juice"));
 console.log(items.includes(45.4));
 
-console.log(items.indexOf('banana'));
+console.log(items.indexOf("banana"));
 console.log(Array.isArray(delowar));
 console.log(Array.isArray(items));
-console.log(items.join('+'));
+console.log(items.join("+"));
