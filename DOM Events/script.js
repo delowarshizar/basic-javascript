cfdd015629360dd93e95b0ae8e92a4c022ff6677
delowarshizar <husain22205101453@diu.edu.bd>
2025-09-20 12:33:55 +0600

    onclick more by get element

---
+++ DOM Events/script.js
@@ -0,0 +1,21 @@
+console.log("file connected successfully");
+
+function makePink()
+{
+    document.body.style.backgroundColor = 'pink';
+}
+
+function makeGreen ()
+{
+    document.body.style.backgroundColor = 'Green';
+}
+
+// option 3
+
+const makePurple = document.getElementById('btn-make-purple');
+// console.log(makePurple);
+makePurple.onclick = makeBgPurple;
+function makeBgPurple()
+{
+    document.body.style.backgroundColor = 'purple'
+}
