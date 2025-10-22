b5ade0450137d2f0f6cdd54817b18493cd3cc3db
delowarshizar <husain22205101453@diu.edu.bd>
2025-10-22 21:52:04 +0600

    final stringh

--- JS6/default-perameter.js
+++ JS6/default-perameter.js
@@ -3,3 +3,10 @@ function add(num1 = 0, num2 = 0) {
   console.log(num1, num2, result);
 }
 add(10, 10);
+
+// string template
+function mul(num3 = 1, num4 = 1) {
+  const result3 = `Multiply of ${num3} and ${num4} equals ${num3 * num4}`;
+  console.log(result3);
+}
+mul(10, 10);
