c5bca1128deab0dbaedb4a860f4958fac7b22794
delowarshizar <husain22205101453@diu.edu.bd>
2025-10-23 00:57:31 +0600

    delowar

--- ES6/spread.js
+++ ES6/spread.js
@@ -9,3 +9,15 @@ console.log(arr2);
 const player = { name: "delowar", age: 39 };
 const playerUpdate = { id: 43, ...player };
 console.log(playerUpdate);
+
+// destructuring
+
+const people = { name: "delowar husain", id: 1453, section: "b" };
+const {name,id,section} = people;
+console.log(name,id,section)
+
+// const {name,id,section} = { name: "delowar husain", id: 1453, section: "b" };
+// console.log(name,id,section)
+
+const [a,b,c,d,e] = [4,4,333,3333,4]
+console.log(a,b,c,d,e)
