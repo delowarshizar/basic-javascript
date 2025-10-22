5c7a2a3da7f9ab1a1a1ab6bd2aeb0afe2830d74b
delowarshizar <husain22205101453@diu.edu.bd>
2025-10-22 23:23:11 +0600

    arrow function used

--- JS6/default-perameter.js
+++ JS6/default-perameter.js
@@ -10,3 +10,17 @@ function mul(num3 = 1, num4 = 1) {
   console.log(result3);
 }
 mul(10, 10);
+
+// arrow function
+const sub = (var1, var2) => var1 - var2;
+
+console.log(sub(3, 4));
+
+const div = (var3, var4) => {
+  const firstDiv = var3 / 3;
+  const secondDiv = var4 / 3;
+  const result = firstDiv + secondDiv;
+  return result;
+};
+
+console.log(div(53, 33));
