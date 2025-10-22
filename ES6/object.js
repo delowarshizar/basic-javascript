60c365a3a514715b3313891ae562cf13ac7d8eb9
delowarshizar <husain22205101453@diu.edu.bd>
2025-10-23 01:13:19 +0600

    object seal and freeze

---
+++ ES6/object.js
@@ -0,0 +1,17 @@
+const employee = {
+  name: "delwoar Husain",
+  id: 1453,
+  Salary: 20000,
+  experience: 1,
+};
+
+console.log(Object.keys(employee));
+console.log(Object.values(employee));
+console.log(Object.entries(employee));
+
+// Object.freeze(employee) cant add or delete or modify
+Object.seal(employee); //can modify but cannt delete anything or add anything
+delete employee.Salary;
+employee.location = "rajshahi";
+employee.name = "Caesar";
+console.log(Object.entries(employee));
