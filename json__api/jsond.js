b71e45a2c685e2f0f67c9ff7a22c27dd78736034
delowarshizar <husain22205101453@diu.edu.bd>
2025-10-28 00:10:35 +0600

    ui load

--- json__api/jsond.js
+++ json__api/jsond.js
@@ -3,3 +3,21 @@ const loadData = () => {
     .then((response) => response.json())
     .then((json) => console.log(json));
 };
+
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      dataDisplay(json);
+    });
+};
+
+const dataDisplay = (posts) => {
+  const postContainer = document.getElementById("post-container");
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    li.innerText = post.title;
+    postContainer.appendChild(li);
+  });
+};
