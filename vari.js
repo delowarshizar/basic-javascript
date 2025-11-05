fe5e65a7afac2e620dacd9871eb4ccd0279f52e7
delowarshizar <husain22205101453@diu.edu.bd>
2025-11-05 17:47:19 +0600

    string templace

--- vari.js
+++ vari.js
@@ -5,3 +5,7 @@ let omi = "omi";
 // delowar = 5 error because we can change; var is also old js
 
 console.log(delowar);
+
+console.log(`want coffe ${delowar} ?`);
+console.log(`want tea ${caesar} ?`);
+console.log(`want milk ${omi} ?`);
